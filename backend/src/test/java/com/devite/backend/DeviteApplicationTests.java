65206f8c441de27fd5a49a527ccaf83f28efa138
zaybournand <zaybournand@gmail.com>
2026-04-26 22:04:47 -0400

    Added docker file

---
+++ backend/src/test/java/com/devite/backend/DeviteApplicationTests.java
@@ -0,0 +1,13 @@
+package com.devite.backend;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class DeviteApplicationTests {
+
+    @Test
+    void contextLoads() {
+    }
+
+}
