41670bf3004b8a296b2b24c1a2226985cc6d4955
zaybournand <zaybournand@gmail.com>
2026-04-26 21:29:39 -0400

    Updated setup

--- backend/src/main/java/com/devite/backend/DeviteApplication.java
+++ backend/src/main/java/com/devite/backend/DeviteApplication.java
@@ -1,4 +1,4 @@
-package com.example.Devite;
+package com.devite.backend;
 
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
@@ -6,8 +6,8 @@ import org.springframework.boot.autoconfigure.SpringBootApplication;
 @SpringBootApplication
 public class DeviteApplication {
 
-	public static void main(String[] args) {
-		SpringApplication.run(DeviteApplication.class, args);
-	}
+    public static void main(String[] args) {
+        SpringApplication.run(DeviteApplication.class, args);
+    }
 
 }
